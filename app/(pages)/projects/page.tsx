48ba18a1e4716437ab38b238a20fb6154ccd9a5d
massimiliano <massimarcello@hotmail.it>
2024-03-15 01:11:50 +0100

    deleted 2 project files

--- app/(pages)/projects/page.tsx
+++ app/(pages)/projects/page.tsx
@@ -1,11 +1,11 @@
 import Projects from '@/components/Projects/Projects'
-import Project2 from '@/components/Projects/Project2';
+
 
 const MyProjects = () => {
   return (
     <div>
         <Projects/>
-        {/* <Project2/> */}
+   
     </div>
   )
 }
