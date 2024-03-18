bac6b3b77ebb08e413bfd84201fff00384745794
massimiliano <massimarcello@hotmail.it>
2024-03-18 17:29:37 +0100

    yo

--- components/Project Home/ProjectFilter.tsx
+++ components/Project Home/ProjectFilter.tsx
@@ -1,78 +1,5 @@
 
 import styled from '@emotion/styled';
-
-
-
-
-
-
-
-
-// import { useState, useEffect } from 'react';
-
-// const ProjectFilter = ({ projects, setFilteredProjects }) => {
-//   const [selectedTechnologies, setSelectedTechnologies] = useState([]);
-//   const [noProjectsMessage, setNoProjectsMessage] = useState(false);
-
-//   const allTechnologies = projects.reduce((technologies, project) => {
-//     project.technologies.forEach(tech => {
-//       if (!technologies.includes(tech)) {
-//         technologies.push(tech);
-//       }
-//     });
-//     return technologies;
-//   }, []);
-
-//   useEffect(() => {
-//     // Filtra i progetti ogni volta che le tecnologie selezionate cambiano
-//     const filteredProjects = projects.filter(project =>
-//       selectedTechnologies.every(tech => project.technologies.includes(tech))
-//     );
-//     setFilteredProjects(filteredProjects);
-
-//     if (filteredProjects.length === 0) {
-//       setNoProjectsMessage(true);
-//     } else {
-//       setNoProjectsMessage(false);
-//     }
-//   }, [selectedTechnologies, projects, setFilteredProjects]);
-
-//   const handleToggleTechnology = (technology) => {
-//     if (selectedTechnologies.includes(technology)) {
-//       setSelectedTechnologies(selectedTechnologies.filter(tech => tech !== technology));
-//     } else {
-//       setSelectedTechnologies([...selectedTechnologies, technology]);
-//     }
-//   };
-
-//   const handleClearSelection = () => {
-//     setSelectedTechnologies([]);
-//   };
-
-//   return (
-//     <Filter>
-//       {allTechnologies.map((technology) => (
-//         <button
-//           key={technology}
-//           onClick={() => handleToggleTechnology(technology)}
-//           style={{ backgroundColor: selectedTechnologies.includes(technology) ? 'blue' : 'gray', color: 'white' }}
-//         >
-//           {technology}
-//         </button>
-//       ))}
-//       <button onClick={handleClearSelection}>Clear</button>
-
-//       {noProjectsMessage && (
-//         <div style={{ backgroundColor: 'red', color: 'white', padding: '10px', marginTop: '10px' }}>
-//         Oops! It looks like there are no projects with this combination of technologies yet.
-//         </div>
-//       )}
-//     </Filter>
-//   );
-// };
-
-// export default ProjectFilter;
-
 import { useState, useEffect } from 'react';
 
 const ProjectFilter = ({ projects, setFilteredProjects, setOpenProjectId }) => {
@@ -133,8 +60,8 @@ const ProjectFilter = ({ projects, setFilteredProjects, setOpenProjectId }) => {
       <ClearButton onClick={handleClearSelection}>Clear</ClearButton>
 
       {noProjectsMessage && (
-        <SorryMessage style={{ backgroundColor: 'red', color: 'white', padding: '10px', marginTop: '30px' }}>
-          Sorry, there are currently no projects with these technologies.
+        <SorryMessage>
+         Oops! It looks like there are no projects with this combination of technologies yet.
         </SorryMessage>
       )}
     </Filter>
@@ -192,7 +119,13 @@ const ClearButton = styled.button`
   }
 `;
 
-const SorryMessage = styled.button``
+const SorryMessage = styled.div`
+background-color: red;
+padding: 1rem;
+
+
+
+`
 
 
 
