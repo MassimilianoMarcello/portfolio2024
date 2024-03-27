a50baac1bc513d27b6ed13eacae4af47577fa3b3
massimiliano <massimarcello@hotmail.it>
2024-03-27 22:37:55 +0100

    jghgf

--- components/Project Home/MediumBlueBorder.tsx
+++ components/Project Home/MediumBlueBorder.tsx
@@ -19,7 +19,10 @@ export default MediumBlueBorder;
 const Section = styled.section`
 margin-top: -6rem;
 padding: 0rem;
+width: 60.6%;
+padding: 0.2rem;
 margin-bottom: 2rem;
+border-top:solid 0.05rem  ${theme.colors.bigYellow} ;
 background-color:${theme.colors.blueDarkest};
  @media (min-width: 1400px) {
   background-color:${theme.colors.blueDarkest} ;
@@ -28,9 +31,9 @@ background-color:${theme.colors.blueDarkest};
 @media (max-width: 600px) {
   margin-bottom: 0rem;
   margin-top: -3rem;
-  padding: 0.2rem;
-  border-bottom:solid .001rem #fff ;
 
+  border-bottom:solid .001rem #fff ;
+  width: 99%;
 }
 
 `
